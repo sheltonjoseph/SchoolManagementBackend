20b03ac2ad7098161a1a0b526cfcbbe1cefdb05e
sheltonjoseph <shelton013@gmail.com>
2022-10-12 14:39:17 +0530

    staff and students api

--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,14 @@
-const Students = require("../models/Student");
 
+const Students = require("../models/Student");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const router = require("express").Router();
 
-router.post("/", async (req, res) => {
+
+// Add Student
+router.post("/",verifyTokenAndAdmin, async (req, res) => {
   const newStudent = new Students(req.body);
   try {
     const savedStudent = await newStudent.save();
@@ -12,8 +18,49 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Edit Student
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedStudent = await Students.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedStudent);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.get("/", async (req, res) => {
+
+// delete a student
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Students.findByIdAndDelete(req.params.id);
+    res.status(200).json("Student has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get SiNGLE Student
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const student = await Students.findById(req.params.id);
+    res.status(200).json(student);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+// Get all Students
+
+router.get("/",verifyTokenAndAuthorization, async (req, res) => {
     try {
       const students = await Students.find();
       res.status(200).json(students);
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -16,7 +16,7 @@ const verifyToken = (req, res, next) => {
 
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.staff.id === req.params.id || req.user.isManagingStaff) {
+    if (req.staff.id === req.params.id || req.staff.isManagingStaff) {
       next();
     } else {
       res.status(403).json("You are not alowed to do that!");
@@ -26,7 +26,7 @@ const verifyTokenAndAuthorization = (req, res, next) => {
 
 const verifyTokenAndAdmin = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.isAdmin) {
+    if (req.staff.isManagingStaff) {
       next();
     } else {
       res.status(403).json("You are not alowed to do that!");
