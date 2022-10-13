222bdf7cc8095850f45076c45ed1add2ba3cf2de
sheltonjoseph <shelton013@gmail.com>
2022-10-13 17:16:54 +0530

    error

--- models/Staff.js
+++ models/Staff.js
@@ -6,13 +6,14 @@ const StaffSchema = new mongoose.Schema(
     lastName: { type: String, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    subjectId:{ type: Array  },
     gender:{type:String , required: true },
     role:{type: String, required: true},
     isManagingStaff: {
       type: Boolean,
       default: false,
     },
+    subjectId:{ type: Array, required: true},
+    img:{ type: String, required: true },
   },
   { timestamps: true }
 );
